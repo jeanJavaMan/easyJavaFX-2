2132094bfa3bea318ae77468de9b6fec885798ca
jeand <jeand@DESKTOP-R65IF3K>
2017-09-20 11:02:29 -0300

    Implementado método para limpeza de campos.

--- easyJavaFX-2/src/main/java/br/jeanderson/util/FunctionAnnotations.java
+++ easyJavaFX-2/src/main/java/br/jeanderson/util/FunctionAnnotations.java
@@ -4,10 +4,13 @@ import java.lang.reflect.Field;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import br.jeanderson.annotations.ClearFields;
+import br.jeanderson.annotations.NotClear;
 import br.jeanderson.annotations.ValidateField;
 import br.jeanderson.control.ControlWindow;
 import br.jeanderson.controller.ValidateMessageController;
 import br.jeanderson.enums.ValidateType;
+import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
@@ -103,4 +106,56 @@ public class FunctionAnnotations {
 		janelaMessage.getController().textoDaMensagem(validate.type().message(validate.fieldName()));
 		janelaMessage.show();
 	}
+	
+	/**
+     * Faz a limpeza do campo de acordo com o seu tipo.
+     *
+     * @param componente Field.
+     */
+    private static void clearField(Object componente) {
+        if (componente instanceof TextInputControl) {
+            ((TextInputControl) componente).setText("");
+        } else if (componente instanceof ComboBox) {
+            ((ComboBox) componente).getSelectionModel().clearSelection();
+        } else if (componente instanceof DatePicker) {
+            ((DatePicker) componente).setValue(null);
+        } else if (componente instanceof ChoiceBox) {
+            ((ChoiceBox) componente).getSelectionModel().clearSelection();
+        } else if (componente instanceof CheckBox) {
+            ((CheckBox) componente).setSelected(false);
+        }
+    }
+
+    private static void clear(Field[] atributos, Object objeto) {
+        try {
+            for (Field atributo : atributos) {
+                if (atributo.isAnnotationPresent(FXML.class)) {
+                    if (!atributo.isAnnotationPresent(NotClear.class)) {
+                        atributo.setAccessible(true);
+                        Object componente = atributo.get(objeto);
+                        FunctionAnnotations.clearField(componente);
+                    }
+                }
+            }
+        } catch (IllegalAccessException | IllegalArgumentException ex) {
+            System.err.println("Houve um erro ao tentar limpar o componente. Exceção: " + ex);
+            Logger.getLogger(FunctionAnnotations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Faz a limpeza de componentes que possue a anotação FXML ou ClearFields.
+     * Obs: funciona somente se a classe tiver a anotação ClearFields.
+     * Para que um campo não seja limpo automáticamente utilize a anotação NotClear neste campo.
+     * @see NotClear
+     * @see ClearFields
+     * @param objeto Classe de controller.
+     */
+    public static void clearFieldsWithAnnotations(Object objeto) {
+        if (objeto.getClass().isAnnotationPresent(ClearFields.class)) {
+            Field[] atributos = objeto.getClass().getDeclaredFields();
+            FunctionAnnotations.clear(atributos, objeto);
+        }
+
+    }
 }
